3c4bca28801c9fd46dbbcdf2be8a8011fe537d41
qkhanhnguyen <nguyenquockhanh13488@gmail.com>
2022-04-02 18:15:56 +0700

    example

--- js/index.js
+++ js/index.js
@@ -6,3 +6,16 @@ function addNewE()
     listTodo.textContent='TODO list';
     document.body.appendChild(listTodo);
 }
+
+import { getDatabase, ref, child, get } from "firebase/database";
+
+const dbRef = ref(getDatabase());
+get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val());
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
